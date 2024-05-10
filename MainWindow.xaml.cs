a2ca6386f85373f10ba5e812b7b4fb2b32cb0c9d
Justin Rinehuls <jrinehuls@gmail.com>
2024-05-10 12:44:37 -0400

    Handle number button clicks

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,73 @@ namespace WpfCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double lastNumber, result;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            equalsButton.Click += EqualsButton_Click;
+            clearButton.Click += ClearButton_Click;
+            negateButton.Click += NegateButton_Click;
+            percentButton.Click += PercentButton_Click;
+
         }
+
+        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+        }
+
+        private void NegateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string value = resultLabel.Content.ToString()!;
+            if (double.TryParse(value, out lastNumber) && lastNumber != 0)
+            {
+                result = lastNumber * -1.0;
+                resultLabel.Content = result.ToString();
+            }
+        }
+
+        private void PercentButton_Click(object sender, RoutedEventArgs e)
+        {
+            string value = resultLabel.Content.ToString()!;
+            if (double.TryParse(value, out lastNumber) && lastNumber != 0)
+            {
+                result = lastNumber / 100.0;
+                resultLabel.Content = result.ToString();
+            }
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            string input;
+
+            if (sender.Equals(zeroButton)) input = "0";
+            else if (sender.Equals(oneButton)) input = "1";
+            else if (sender.Equals(twoButton)) input = "2";
+            else if (sender.Equals(threeButton)) input = "3";
+            else if (sender.Equals(fourButton)) input = "4";
+            else if (sender.Equals(fiveButton)) input = "5";
+            else if (sender.Equals(sixButton)) input = "6";
+            else if (sender.Equals(sevenButton)) input = "7";
+            else if (sender.Equals(eightButton)) input = "8";
+            else if (sender.Equals(nineButton)) input = "9";
+            else return;
+
+            if (resultLabel.Content.ToString()!.Equals("0"))
+            {
+                resultLabel.Content = input;
+            }
+            else {
+                resultLabel.Content += input;
+            }
+        }
+
     }
 }
